adb0a4087bb6765c664e6ac013fde87c0cf3bbc6
litsazib <roktimsazib92@gmail.com>
2023-04-09 14:29:30 +0600

    modify

--- src/components/RestaurantPos/GrandTotal/GrandTotal.js
+++ src/components/RestaurantPos/GrandTotal/GrandTotal.js
@@ -31,7 +31,7 @@ const GrandTotal = ({ invoiceData, totalDiscount, totalAmount, customerType, cus
 		if (!_.isNil(orderData)) {
 			let { trans_service, trans_tax } = orderData;
 			setServiceChargeByPercent(trans_service);
-			setTaxByPercent(trans_tax);
+			setTaxByPercent(15);
 		}
 	}, [orderData]);
 
@@ -44,7 +44,7 @@ const GrandTotal = ({ invoiceData, totalDiscount, totalAmount, customerType, cus
 	const calcGrandTotal = () => {
 		let calcTaxChargeAmount = (totalAmount / 100) * taxByPercent;
 		let calcServiceChargeAmount = (totalAmount / 100) * serviceChargeByPercent;
-		setTaxChargeAmount(calcTaxChargeAmount);
+		setTaxChargeAmount(Math.round(calcTaxChargeAmount));
 		setServiceChargeAmount(calcServiceChargeAmount);
 		setGrandTotalAmount(() => {
 			return Math.round(totalAmount + calcTaxChargeAmount + calcServiceChargeAmount);
@@ -208,7 +208,7 @@ const GrandTotal = ({ invoiceData, totalDiscount, totalAmount, customerType, cus
 						<div className="row align-items-center">
 							<div className="col-sm-6 col-xl-4 mb-2 mb-md-0">
 								<div className="overview-widget d-flex align-items-center justify-content-md-end gap-3 mb-1">
-									<p className="mb-0 flex-shrink-0">Vat/Tax({taxByPercent}%) :</p>
+									<p className="mb-0 flex-shrink-0">Vat({taxByPercent}%) :</p>
 									<span style={{ width: '120px' }} className="form-control form-control-sm">
 										{currency_position === 'prefix' && currency_symbol} {Number(taxChargeAmount).toFixed(2)} {currency_position === 'suffix' && currency_symbol}
 									</span>
