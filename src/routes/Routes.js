923afed06a1aa8b6b74beb660eb3cab2f7159f1e
litsazib <roktimsazib92@gmail.com>
2023-05-08 09:03:49 +0600

    s

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -23,11 +23,7 @@ import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 
 const Routes = () => {
-	/**
-	 * @method {useSelector}
-	 * @param  {} => reduxStore
-	 * To get the global redux store
-	 */
+
 	const posAccessPermissions = useSelector((reduxStore) => {
 		let { pos_permission_access } = reduxStore?.userReducer?.user;
 		let posPermissions = !_.isNil(pos_permission_access) && pos_permission_access.split(',');
@@ -47,24 +43,6 @@ const Routes = () => {
 					<Route path="/current-orders" element={<COrders />} />
 
 					<Route path="" element={<CardPos />} />
-
-
-				{/* {!_.isNil(posAccessPermissions) &&
-					_.size(posAccessPermissions) > 0 &&
-					posAccessPermissions.map((pos_name, index) => {
-						return (
-							<Route key={index} path={`/${pos_name}-pos`} element={<ProtectedRoute />}>
-								{pos_name === 'restaurant' && <Route path="" element={<RestaurantPos />} />}
-								{pos_name === 'restaurant' && <Route path="current-orders" element={<CurrentOrders />} />}
-
-								{pos_name === 'room-service' && <Route path="" element={<RoomServicePos />} />}
-								{pos_name === 'room-service' && <Route path="current-orders" element={<COrders />} />}
-
-								{pos_name === 'card' && <Route path="" element={<CardPos />} />}
-							</Route>
-						);
-					})} */}
-
 				<Route path="/login" element={<Login />} />
 				<Route path="*" element={<NotFound />} />
 			</PathWays>
